76a9f408b7fc0bd498adce693e1dafd037d01ed3
leost3 <leost3@gmail.com>
2019-05-22 20:54:42 -0400

    imaged added from local imaged to UI

--- src/components/CitiesList.js
+++ src/components/CitiesList.js
@@ -1,11 +1,12 @@
 import React from 'react';
 // import City from './City';
+import toronto from '../photos/Toronto.jpg'
 
 const CitiesList = ({cities}) => {
     const cityArr = cities.map (city => {
         return (
             <div>
-                <img src="photos/calgary.jpg" alt={city.city} />
+                <img src={`/photos/${city.city}.jpg`} alt={city.city} />
                 <h1>{city.city}</h1>
                 <h1>{city.admin}</h1>
             </div>
